c8625bdc69b7a7db4760e9ee63f00a238aa089d2
João Junior <batistasousajoao@gmail.com>
2023-12-27 16:38:06 -0300

    Add project files.

---
+++ ProductExercise/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using ProductExercise.Entities;
+
+namespace Course
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n ; i++)
+            {
+                Console.WriteLine($"Product #{i} data:");
+                Console.WriteLine("Common, used or imported? (c/u/i) ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (ch == 'c')
+                {
+                    list.Add(new Product(name, price));
+                }
+                else if (ch == 'u')
+                {
+                    Console.Write("Manufacture Date (DD/MM/YYYY): ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    list.Add(new UsedProduct(name, price, date));
+                }
+                else
+                {
+                    Console.Write("Customs fee: $ ");
+                    double customsFee = double.Parse(Console.ReadLine());
+                    list.Add(new ImportedProduct(name, price, customsFee));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PRICES TAGS");
+
+            foreach(Product prod in list)
+            {
+                Console.WriteLine(prod.PriceTag());
+            }
+        }
+    }
+}
